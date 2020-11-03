8b27781b5dd2d5f967741e7cd7b43bf2353af748
William-Moreno <intrcptr@comcast.net>
2020-11-03 08:01:34 -0800

    created a 6th loction to try a method inside an object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// var locations = ['seattle','tokyo','dubai','paris','lima'];
+// var locations = [seattle,tokyo,dubai,paris,lima];
 
 function populate(location) {
   for( var i = 0 ; i < 14 ; i++ ) {
@@ -19,12 +19,12 @@ function populate(location) {
   location.totalCookiesDaily = totalSales;
 }
 
-function hourlySales(location) {
+function dailySales(location) {
   var times = ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'];
   for( var i = 0 ; i < location.cookiesSoldPerHour.length ; i++ ) {
-  var liElement = document.createElement('li');
-  liElement.textContent = times[i] + ' ' + location.cookiesSoldPerHour[i] + ' cookies';
-  location.list.appendChild(liElement);
+    var liElement = document.createElement('li');
+    liElement.textContent = times[i] + ' ' + location.cookiesSoldPerHour[i] + ' cookies';
+    location.list.appendChild(liElement);
   }
   var liElement = document.createElement('li');
   liElement.textContent = 'Total: ' + location.totalCookiesDaily + ' cookies';
@@ -37,8 +37,7 @@ var seattle = {
   maxCustomers: 65,
   cookiesPerCustomer: 6.3,
   customersPerHour: [],
-  cookiesSoldPerHour: [],
-  totalCookiesDaily: 0
+  cookiesSoldPerHour: []
 }
 
 var tokyo = {
@@ -47,8 +46,7 @@ var tokyo = {
   maxCustomers: 24,
   cookiesPerCustomer: 1.2,
   customersPerHour: [],
-  cookiesSoldPerHour: [],
-  totalCookiesDaily: 0
+  cookiesSoldPerHour: []
 }
 
 var dubai = {
@@ -57,8 +55,7 @@ var dubai = {
   maxCustomers: 38,
   cookiesPerCustomer: 3.7,
   customersPerHour: [],
-  cookiesSoldPerHour: [],
-  totalCookiesDaily: 0
+  cookiesSoldPerHour: []
 }
 
 var paris = {
@@ -67,8 +64,7 @@ var paris = {
   maxCustomers: 38,
   cookiesPerCustomer: 2.3,
   customersPerHour: [],
-  cookiesSoldPerHour: [],
-  totalCookiesDaily: 0
+  cookiesSoldPerHour: []
 }
 
 var lima = {
@@ -77,8 +73,7 @@ var lima = {
   maxCustomers: 16,
   cookiesPerCustomer: 4.6,
   customersPerHour: [],
-  cookiesSoldPerHour: [],
-  totalCookiesDaily: 0
+  cookiesSoldPerHour: []
 }
 
 /* populate the object arrays */
@@ -91,9 +86,38 @@ populate(lima);
 
 /* write lists to sales sheet */
 
-hourlySales(seattle);
-hourlySales(tokyo);
-hourlySales(dubai);
-hourlySales(paris);
-hourlySales(lima);
+dailySales(seattle);
+dailySales(tokyo);
+dailySales(dubai);
+dailySales(paris);
+dailySales(lima);
+
+/* testing another location for methods */
+
+var venice = {
+  list: document.getElementById('venice-list'),
+  minCustomers: 17,
+  maxCustomers: 32,
+  cookiesPerCustomer: 5.2,
+  customersPerHour: [],
+  cookiesSoldPerHour: [],
+  totalDailyCookies: 0,
+  hourlyData: function () {
+    for( var i = 0 ; i < 14 ; i++ ) {
+      var min = Math.ceil(this.minCustomers);
+      var max = Math.floor(this.maxCustomers);
+      this.customersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
+    }
+    for( var j = 0 ; j < 14 ; j++ ) {
+      var cookieTotal = Math.ceil(this.customersPerHour[j] * this.cookiesPerCustomer);
+      this.cookiesSoldPerHour.push(cookieTotal);
+    }
+    for( var k = 1 ; k < 14 ; k++ ) {
+      this.totalDailyCookies += this.cookiesSoldPerHour[k];
+    }
+  }
+}
+
+venice.hourlyData();
+console.log(venice);
 
