9b240e7d25b912a2b412e1360cf564615c3660c3
William-Moreno <intrcptr@comcast.net>
2020-11-02 18:35:33 -0800

    commented out an array I am not yet using

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var locations = ['seattle','tokyo','dubai','paris','lima'];
+// var locations = ['seattle','tokyo','dubai','paris','lima'];
 var times = ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'];
 
 function populate(location) {
